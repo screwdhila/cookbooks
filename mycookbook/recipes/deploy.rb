a34e467efef805860c119eb73bef99b769b65bbf
Maruful Hassan <otarini@gmail.com>
2016-10-07 23:20:21 +1100

    remove app information

--- mycookbook/recipes/deploy.rb
+++ mycookbook/recipes/deploy.rb
@@ -9,10 +9,6 @@ instance = search("aws_opsworks_instance").first
 
 mydatabag = search("mybag1").first
 
-app1 = search("aws_opsworks_app","shortname:app1").first
-app2 = search("aws_opsworks_app","shortname:app2").first
-app3 = search("aws_opsworks_app","shortname:app3").first
-
 template "#{apache_docroot}/index.html" do
 	source "index.html.erb"
 	mode 0644
@@ -22,8 +18,5 @@ template "#{apache_docroot}/index.html" do
 		:stack => stack,
 		:instance => instance,
 		:mydatabag => mydatabag,
-		:app1 => app1['deploy'],
-		:app2 => app2['deploy'],
-		:app3 => app3['deploy']
-	)
+		)
 end
